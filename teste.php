64873f92d55d123c0dbf09ee7b2262bdda33fbd2
yclone <vinicius_marra@hotmail.com>
2016-12-06 12:15:26 -0200

    novo

---
+++ teste.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class teste{
+    var $servername = "localhost";
+    var $username = "yclone";
+    var $password = "";
+    var $dbname = "fipe";
+    var $query;
+    var $link;
+    var $conn;
+    var $result;
+    var $db;
+    function teste(){
+
+
+
+   $servername = "";
+   $username = "";
+   $password = "";
+   $dbname = "";
+    }
+
+
+?>
